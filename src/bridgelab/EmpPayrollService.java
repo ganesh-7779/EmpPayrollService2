d9aee210e0a6a4bb1cc816c19ad527749c138fd9
ganesh7779 <ganeshgavhad9@gmail.com>
2023-02-21 20:29:26 +0530

    Welcome to the employee payroll service

---
+++ src/bridgelab/EmpPayrollService.java
@@ -0,0 +1,11 @@
+package bridgelab;
+
+public class EmpPayrollService {
+	
+	public static void main (String args []) {
+		
+		System.out.println("Welcome to employee payroll service");
+		
+	}
+	
+}
