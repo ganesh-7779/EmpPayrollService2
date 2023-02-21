9fe050ad295028a43ef386e70cf1b0acd88789a6
ganesh7779 <ganeshgavhad9@gmail.com>
2023-02-21 21:11:00 +0530

    Added: UC2 employee dailyWage

--- src/bridgelab/EmpPayrollService.java
+++ src/bridgelab/EmpPayrollService.java
@@ -11,18 +11,19 @@ public class EmpPayrollService {
 		
 		//UC1=> Check emp is present or not
 		//Branch => empCheck_UC1
+		
+		//UC2 calculate daily emplyee wage
+		// WagePerHrs = 20 FulldayHrs=8
 		Random random = new Random();
-		
 		int empCheck = random.nextInt(2);
-		
+		int WagePerHrs = 20;
+		int FullDayHrs = 8;
 		if(empCheck==0) {
 			System.out.println("Employee is Absent");
 		}else {
 			System.out.println("Employee is Present");
+			int dailyWage = WagePerHrs*FullDayHrs;
+			System.out.println("Employee Daily Wage is "+dailyWage);
 		}
-		
-		//0 => absent // 1=> present
-		
-	}
-	
+	}	
 }
