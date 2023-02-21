cb7ab77fd1971d3b876b42b0b83aca22b4126c63
ganesh7779 <ganeshgavhad9@gmail.com>
2023-02-21 21:31:20 +0530

    Added: UC3 employee PartTimeWage

--- src/bridgelab/EmpPayrollService.java
+++ src/bridgelab/EmpPayrollService.java
@@ -13,14 +13,22 @@ public class EmpPayrollService {
 		//Branch => empCheck_UC1
 		
 		//UC2 calculate daily emplyee wage
-		// WagePerHrs = 20 FulldayHrs=8
+		// WagePerHrs = 20 FulldayHrs=8 PartTimeHrs =4;
 		Random random = new Random();
-		int empCheck = random.nextInt(2);
+		int empCheck = random.nextInt(3);//0,1,2
 		int WagePerHrs = 20;
 		int FullDayHrs = 8;
+		int PartTimeHrs =4;
+		
 		if(empCheck==0) {
 			System.out.println("Employee is Absent");
-		}else {
+		// UC_3 Part Time	
+		}else if(empCheck==1) {			
+			System.out.println("Employee is Present but part time");
+			int dailyWage = WagePerHrs*PartTimeHrs;
+			System.out.println("Employee Daily Wage is "+dailyWage);		
+		}
+		else {
 			System.out.println("Employee is Present");
 			int dailyWage = WagePerHrs*FullDayHrs;
 			System.out.println("Employee Daily Wage is "+dailyWage);
