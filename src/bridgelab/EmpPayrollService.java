2a988604842aadc43e906df132bff956fd9497ea
ganesh7779 <ganeshgavhad9@gmail.com>
2023-02-21 20:51:48 +0530

    Added: UC1 employee check

--- src/bridgelab/EmpPayrollService.java
+++ src/bridgelab/EmpPayrollService.java
@@ -1,10 +1,27 @@
 package bridgelab;
 
+import java.util.Random;
+
+
 public class EmpPayrollService {
 	
 	public static void main (String args []) {
 		
 		System.out.println("Welcome to employee payroll service");
+		
+		//UC1=> Check emp is present or not
+		//Branch => empCheck_UC1
+		Random random = new Random();
+		
+		int empCheck = random.nextInt(2);
+		
+		if(empCheck==0) {
+			System.out.println("Employee is Absent");
+		}else {
+			System.out.println("Employee is Present");
+		}
+		
+		//0 => absent // 1=> present
 		
 	}
 	
